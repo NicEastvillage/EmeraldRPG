768afcae64cf312f719c191e5932df743dc1ba85
NicEastvillage <nicoesterby@gmail.com>
2018-05-12 20:54:35 +0200

    Created project.

---
+++ desktop/src/com/eastvillage/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eastvillage.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eastvillage.EmeraldGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EmeraldGame(), config);
+	}
+}
