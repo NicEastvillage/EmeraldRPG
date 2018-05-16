4693cbcb3ec11142809b72a540960714d4b51f57
NicEastvillage <nicoesterby@gmail.com>
2018-05-16 18:11:26 +0200

    Tiles now uses AssetManager.

--- core/src/com/eastvillage/emerald/battlefield/Tile.java
+++ core/src/com/eastvillage/emerald/battlefield/Tile.java
@@ -1,6 +1,9 @@
 package com.eastvillage.emerald.battlefield;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.eastvillage.emerald.Assets;
+import com.eastvillage.emerald.EmeraldGame;
 import com.eastvillage.engine.GameObject;
 import com.eastvillage.engine.TexRenderer;
 import com.eastvillage.engine.TransformTree;
@@ -13,14 +16,13 @@ public class Tile extends GameObject {
     public static final int SPACING_WIDTH = PIX_WIDTH;
     public static final int SPACING_HEIGHT = PIX_HEIGHT - 8;
 
-    private static final Texture texture = new Texture("grass.png");
-
     public final Hex hex;
 
     public Tile(Hex hex, TransformTree<GameObject> parent) {
         this.hex = hex;
         transform.setParent(parent);
         transform.setWorldPosition(new Vector2(hex.x * SPACING_WIDTH + hex.y * SPACING_WIDTH / 2, hex.y * SPACING_HEIGHT));
+        TextureRegion texture = new TextureRegion((Texture) EmeraldGame.getAsset(Assets.GRASS));
         addComponent(new TexRenderer(texture, false));
     }
 }
