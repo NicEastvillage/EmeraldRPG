214b3df2ddea153ef64443fdae2dafe230d1c5fc
NicEastvillage <nicoesterby@gmail.com>
2018-05-27 13:57:52 +0200

    Removed TexRenderers should dispose option, and added comments.

--- core/src/com/eastvillage/engine/TexRenderer.java
+++ core/src/com/eastvillage/engine/TexRenderer.java
@@ -5,41 +5,46 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.eastvillage.utility.math.Vector2;
 
+/** The TexRenderer component is used to draw textures or texture regions. It supports rotation, scaling and layers. */
 public final class TexRenderer implements Component, ZDrawable {
 
     private boolean enabled = true;
 
     private TransformTree<GameObject> transform;
     private Vector2 scale = Vector2.ONE;
-    private TextureRegion texReg;
-    private boolean shouldDispose;
+    private TextureRegion tex;
     private int z = 0;
 
-    public TexRenderer(TransformTree<GameObject> transform, Texture texture, boolean shouldDispose) {
-        this(transform, new TextureRegion(texture), shouldDispose);
+    /** The TexRenderer component is used to draw textures or texture regions. It supports rotation, scaling and layers. */
+    public TexRenderer(TransformTree<GameObject> transform, Texture texture) {
+        this(transform, new TextureRegion(texture));
     }
 
-    public TexRenderer(TransformTree<GameObject> transform, TextureRegion texReg, boolean shouldDispose) {
-        this(transform, texReg, Vector2.ONE, shouldDispose);
+    /** The TexRenderer component is used to draw textures or texture regions. It supports rotation, scaling and layers. */
+    public TexRenderer(TransformTree<GameObject> transform, TextureRegion tex) {
+        this(transform, tex, Vector2.ONE);
     }
 
-    public TexRenderer(TransformTree<GameObject> transform, Texture texture, float scaleX, float scaleY, boolean shouldDispose) {
-        this(transform, new TextureRegion(texture), scaleX, scaleY, shouldDispose);
+    /** The TexRenderer component is used to draw textures or texture regions. It supports rotation, scaling and layers. */
+    public TexRenderer(TransformTree<GameObject> transform, Texture texture, float scaleX, float scaleY) {
+        this(transform, new TextureRegion(texture), scaleX, scaleY);
     }
 
-    public TexRenderer(TransformTree<GameObject> transform, TextureRegion texReg, float scaleX, float scaleY, boolean shouldDispose) {
-        this(transform, texReg, new Vector2(scaleX, scaleY), shouldDispose);
+    /** The TexRenderer component is used to draw textures or texture regions. It supports rotation, scaling and layers. */
+    public TexRenderer(TransformTree<GameObject> transform, TextureRegion tex, float scaleX, float scaleY) {
+        this(transform, tex, new Vector2(scaleX, scaleY));
     }
 
-    public TexRenderer(TransformTree<GameObject> transform, Texture texture, Vector2 scale, boolean shouldDispose) {
-        this(transform, new TextureRegion(texture), scale, shouldDispose);
+    /** The TexRenderer component is used to draw textures or texture regions. It supports rotation, scaling and layers. */
+    public TexRenderer(TransformTree<GameObject> transform, Texture texture, Vector2 scale) {
+        this(transform, new TextureRegion(texture), scale);
     }
 
-    public TexRenderer(TransformTree<GameObject> transform, TextureRegion texReg, Vector2 scale, boolean shouldDispose) {
+    /** The TexRenderer component is used to draw textures or texture regions. It supports rotation, scaling and layers. */
+    public TexRenderer(TransformTree<GameObject> transform, TextureRegion tex, Vector2 scale) {
         this.transform = transform;
         this.scale = scale;
-        this.texReg = new TextureRegion(texReg);
-        this.shouldDispose = shouldDispose;
+        this.tex = tex;
     }
 
     @Override
@@ -54,14 +59,14 @@ public final class TexRenderer implements Component, ZDrawable {
 
     @Override
     public void draw(SpriteBatch batch) {
-        if (texReg != null) {
+        if (tex != null) {
             Vector2 position = transform.getWorldPosition();
             float rotation = transform.getWorldRotation();
-            float sprWidth = texReg.getRegionWidth();
-            float sprHeight = texReg.getRegionHeight();
+            float sprWidth = tex.getRegionWidth();
+            float sprHeight = tex.getRegionHeight();
             float originX = sprWidth * 0.5f;
             float originY = sprHeight * 0.5f;
-            batch.draw(texReg, position.x - originX, position.y - originY, originX, originY, sprWidth, sprHeight, scale.x, scale.y, (float)Math.toDegrees(rotation));
+            batch.draw(tex, position.x - originX, position.y - originY, originX, originY, sprWidth, sprHeight, scale.x, scale.y, (float)Math.toDegrees(rotation));
         }
     }
 
@@ -70,15 +75,14 @@ public final class TexRenderer implements Component, ZDrawable {
         return z;
     }
 
+    /** Set the z value of this texture. Textures with high z value will be drawn on top of textures with low z value. */
     public void setZ(int z) {
         this.z = z;
     }
 
     @Override
     public void dispose() {
-        if (shouldDispose) {
-            texReg.getTexture().dispose();
-        }
+
     }
 
     @Override
@@ -91,32 +95,34 @@ public final class TexRenderer implements Component, ZDrawable {
         return enabled;
     }
 
+    /** Returns the scaling of the drawn texture. */
     public Vector2 getScale() {
         return scale;
     }
 
+    /** Scale the drawn texture. */
     public void setScale(Vector2 scale) {
         if (scale == null) throw new IllegalArgumentException("Scale cannot be null.");
         this.scale = scale;
     }
 
-    public TextureRegion getTextureRegion() {
-        return texReg;
+    /** Returns the texture region */
+    public TextureRegion getTex() {
+        return tex;
     }
 
-    public void setTex(Texture texture, boolean shouldDispose) {
-        setTex(new TextureRegion(texture), shouldDispose);
+    /** Change the drawn texture. */
+    public void setTex(Texture texture) {
+        setTex(new TextureRegion(texture));
     }
 
-    public void setTex(TextureRegion sprite, boolean shouldDispose) {
-        if (this.texReg != null && this.shouldDispose) {
-            this.texReg.getTexture().dispose();
-        }
-        this.texReg = sprite;
-        this.shouldDispose = shouldDispose;
+    /** Change the drawn texture region. */
+    public void setTex(TextureRegion tex) {
+        this.tex = tex;
     }
 
-    public void flipTex(boolean x, boolean y) {
-        texReg.flip(x, y);
+    /** Flip the texture horizontally and/or vertically. */
+    public void flipTex(boolean horizontally, boolean vertically) {
+        tex.flip(horizontally, vertically);
     }
 }
--- core/src/com/eastvillage/engine/ZDrawable.java
+++ core/src/com/eastvillage/engine/ZDrawable.java
@@ -5,5 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public interface ZDrawable {
 
     void draw(SpriteBatch batch);
+
+    /** Get the z value of this ZDrawable. Drawables with high z value will be drawn on top of Drawables with low z value. */
     int getZ();
 }
