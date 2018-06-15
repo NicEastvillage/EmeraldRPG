d1f617226f7c00430c5db8c82dec28c7a1f7e7a6
NicEastvillage <nicoesterby@gmail.com>
2018-06-15 09:18:50 +0200

    SpellController can now differenciate different types of spells.

--- core/src/com/eastvillage/emerald/battle/SpellController.java
+++ core/src/com/eastvillage/emerald/battle/SpellController.java
@@ -6,10 +6,16 @@ import com.eastvillage.emerald.battle.battlefield.ClickableHighlightController;
 import com.eastvillage.emerald.battle.turn.TurnController;
 import com.eastvillage.emerald.battle.turn.TurnQueueListener;
 import com.eastvillage.emerald.spells.Ability;
+import com.eastvillage.emerald.spells.Spell;
+import com.eastvillage.emerald.spells.TargetedSpell;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/** The SpellController keeps track of which spells the current unit can cast as well as selecting, deselecting, and casting. */
 public class SpellController implements TurnQueueListener {
+
+    private static final int NONE = -1;
 
     private BattleController controller;
     private Battlefield battlefield;
@@ -17,6 +23,8 @@ public class SpellController implements TurnQueueListener {
 
     private BattleUnit currentUnit;
     private List<Ability> currentAbilities;
+    private int selectedIndex = NONE;
+    private Ability selectedAbility;
 
     public SpellController(BattleController controller) {
         this.controller = controller;
@@ -26,6 +34,7 @@ public class SpellController implements TurnQueueListener {
 
     @Override
     public void onQueueCycle(TurnController turnController) {
+        deselect();
         currentUnit = turnController.current().getUnit();
         currentAbilities = currentUnit.getAbilities();
     }
@@ -33,5 +42,43 @@ public class SpellController implements TurnQueueListener {
     @Override
     public void onQueueModified(TurnController turnController) {
         
+    }
+
+    public List<Ability> getAbilities() {
+        return new ArrayList<>(currentAbilities);
+    }
+
+    /** Activate an ability given an index. This will either do nothing, cast the spell, or prompt for targeting
+     * depending on what type of spell is selected. If the activated index is currently selected, it will be
+     * deselected. */
+    public void activate(int index) {
+        if (index < 0 || currentAbilities.size() <= index) throw new IndexOutOfBoundsException();
+
+        if (selectedIndex == index) {
+            deselect();
+        } else {
+            Ability activated = currentAbilities.get(index);
+            if (activated instanceof TargetedSpell) {
+                selectSpellAndStartTargeting(index, (TargetedSpell) activated);
+            } else if (activated instanceof Spell) {
+                // instant spell // TODO
+            } else {
+                // passive or aura // TODO
+            }
+        }
+    }
+
+    private void selectSpellAndStartTargeting(int index, TargetedSpell spell) {
+        selectedIndex = index;
+        selectedAbility = spell;
+        spell.onTargetSelectBegin(battlefield, clickableHighlightController);
+    }
+
+    /** Deselect any currently selected TargetedSpell. */
+    public void deselect() {
+        // Only TargetedSpells can be "selected" currently
+        if (selectedAbility != null) ((TargetedSpell) selectedAbility).onTargetSelectCancel(clickableHighlightController);
+        selectedIndex = NONE;
+        selectedAbility = null;
     }
 }
